e61e0ee159d26d9b5449dae501ab0827706b0b09
EddyDezuraud <eddy.dezuraud@gmail.com>
2024-03-18 22:33:35 +0100

    Feat: About page

---
+++ pages/about.vue
@@ -0,0 +1,17 @@
+<template>
+    <ContentDoc v-slot="{ doc }" >
+        <PageHeader />
+        <section>
+            <PageHeading 
+                v-if="doc.heading"
+                :label="doc.heading.label" 
+                :title="doc.heading.title" 
+                :subtitle="doc.heading.description" 
+                button
+            />
+        </section>
+        <section>
+        </section>
+    </ContentDoc>
+</template>
+
